d4d9ced92953d6e18d4727819702d0132b796535
gonzalojmartin <gonzalojmartin@hotmail.com>
2015-08-28 22:10:43 -0300

    Luchadores Sumo

---
+++ TPs/LuchadoresJaponeses/src/edu/unlam/luchadoresjaponeses/Sumo.java
@@ -0,0 +1,85 @@
+package edu.unlam.luchadoresjaponeses;
+
+public class Sumo implements Comparable<Sumo>{
+
+	private int peso;
+	private int altura;
+	private int victimas;
+
+	public Sumo(int peso, int altura) {
+		super();
+		this.peso = peso;
+		this.altura = altura;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getVictimas() {
+		return victimas;
+	}
+
+	public void setVictimas(int victimas) {
+		this.victimas = victimas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + altura;
+		result = prime * result + peso;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sumo other = (Sumo) obj;
+		if (altura != other.altura)
+			return false;
+		if (peso != other.peso)
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Sumo sumo) {
+		boolean superaEnAltura = this.altura > sumo.altura;
+		boolean superaEnPeso = this.peso > sumo.peso;
+		
+		if(superaEnAltura && superaEnPeso){
+			return 1;
+		}else if( (superaEnAltura && !superaEnPeso) || (superaEnPeso && !superaEnAltura) ){
+			return 0;
+		}else if(!superaEnAltura && !superaEnPeso){
+			return -1;
+		}else{
+			// Lo supera en peso o altura, y el otro es igual
+			return 1;
+		}
+	}
+
+	public void addVictima() {
+		this.victimas++;
+	}
+	
+}
