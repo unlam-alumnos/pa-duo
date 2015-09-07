d7f895a8b228a5439dd2f6b49e64855a887471c6
gonzalojmartin <gonzalojmartin@hotmail.com>
2015-09-07 16:32:20 -0300

    matriz, inversa

--- Practica_Clase/Math/src/model/MatrizMath.java
+++ Practica_Clase/Math/src/model/MatrizMath.java
@@ -109,6 +109,54 @@ public class MatrizMath {
         return determinante;		
 	}
 	
+	public MatrizMath getInversa() throws Exception {
+
+		if (this.getDeterminante() == 0)
+			throw new Exception("La matriz no posee inversa");
+
+		int i, j, k;
+		double escalar;
+		MatrizMath aux = this.clone();
+		MatrizMath inversa = new MatrizMath(this.filas, this.columnas);
+
+		for (i = 0; i < this.filas; i++)
+			inversa.add(i, i, 1);
+
+		for (k = 0; k < this.filas; k++) {
+			if (aux.getCoordMatriz(k, k) == 0) {
+				i = k;
+				while (aux.getCoordMatriz(i, k) == 0 && i < this.filas - k)
+					i++;
+				for (j = 0; j < this.filas; j++) {
+					aux.add(k, j, aux.getCoordMatriz(k, j) + aux.getCoordMatriz(i, j));
+					inversa.add(k, j, inversa.getCoordMatriz(k,j) + inversa.getCoordMatriz(i, j));
+				}
+			}
+			escalar = aux.getCoordMatriz(k, k);
+			for (j = 0; j < this.filas; j++) {
+				aux.add(k, j, aux.getCoordMatriz(k, j) * (1 / escalar));
+				inversa.add(k, j, inversa.getCoordMatriz(k, j) * (1 / escalar));
+			}
+			for (i = k + 1; i < this.filas; i++) {
+				escalar = aux.getCoordMatriz(i, k);
+				for (j = 0; j < this.filas; j++) {
+					aux.add(i, j, aux.getCoordMatriz(i, j) - escalar * aux.getCoordMatriz(k, j));
+					inversa.add(i, j, inversa.getCoordMatriz(i, j) - escalar * inversa.getCoordMatriz(k, j));
+				}
+			}
+		}
+		for (k = 0; k < this.filas - 1; k++){
+			for (i = 1; i < this.filas - k; i++) {
+				escalar = aux.getCoordMatriz(k, k + i);
+				for (j = 0; j < this.filas; j++) {
+					aux.add(k, j, aux.getCoordMatriz(k, j) - escalar * aux.getCoordMatriz(k + i, j));
+					inversa.add(k, j, inversa.getCoordMatriz(k, j) - escalar * inversa.getCoordMatriz(k + i, j));
+				}
+			}
+		}
+		return inversa;
+	}
+	
 	public MatrizMath clone() {
 		MatrizMath aux = new MatrizMath(filas,columnas);
 
@@ -166,8 +214,8 @@ public class MatrizMath {
 	public double[][] getMatriz() {
 		return matriz;
 	}
-
-	public double getCoordMatriz(int f, int c) {
+	
+	public double getCoordMatriz(int f, int c){
 		return matriz[f][c];
 	}
 }
